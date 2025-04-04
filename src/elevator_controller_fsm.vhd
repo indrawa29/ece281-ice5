019bc4f7e461834a9c4cace978b55622e5c32b56
indrawa29 <c27indra.adimatshara@afacademy.af.edu>
2025-04-04 16:37:42 -0600

    trial1

--- src/elevator_controller_fsm.vhd
+++ src/elevator_controller_fsm.vhd
@@ -82,7 +82,7 @@ end elevator_controller_fsm;
  
 architecture Behavioral of elevator_controller_fsm is
 
-    -- Below you create a new variable type! You also define what values that 
+    -- Below you create a new variable type! You also define what values that
     -- variable type can take on. Now you can assign a signal as 
     -- "sm_floor" the same way you'd assign a signal as std_logic
 	type sm_floor is (s_floor1, s_floor2, s_floor3, s_floor4);
@@ -95,15 +95,43 @@ begin
 	-- CONCURRENT STATEMENTS ------------------------------------------------------------------------------
 	
 	-- Next State Logic
+	-- Next State Logic
+      f_Q_next <= s_floor2 when (f_Q = s_floor1 and i_up_down = '1') else
+                  s_floor3 when (f_Q = s_floor2 and i_up_down = '1') else -- going up
+                  s_floor4 when (f_Q = s_floor3 and i_up_down = '1') else
+                  s_floor4 when (f_Q = s_floor4 and i_up_down = '1') else
+            -- going down
+                  s_floor1 when (f_Q = s_floor2 and i_up_down = '0') else
+                  s_floor2 when (f_Q = s_floor3 and i_up_down = '0') else
+                  s_floor3 when (f_Q = s_floor4 and i_up_down = '0') else
+                  s_floor1 when (f_Q = s_floor1 and i_up_down = '0') else
+          
+                  s_floor2
+            ; -- default case
   
 	-- Output logic
+    with f_Q select
+    o_floor <= "0001" when s_floor1,
+               "0010" when s_floor2,
+               "0011" when s_floor3,
+               "0100" when s_floor4,
+               "0010" when others; -- default is floor1   
 
 	-------------------------------------------------------------------------------------------------------
 	
 	-- PROCESSES ------------------------------------------------------------------------------------------	
-	
 	-- State register ------------
 	
+	register_proc : process (i_clk, i_reset)
+    begin
+        if i_reset = '1' then
+            f_Q <= s_floor2; 
+        elsif i_stop = '1' then
+            f_Q <= f_Q;
+        elsif (rising_edge(i_clk)) then
+            f_Q <= f_Q_next;    -- next state becomes current state
+        end if;
+    end process register_proc;
 	
 	-------------------------------------------------------------------------------------------------------
 	
